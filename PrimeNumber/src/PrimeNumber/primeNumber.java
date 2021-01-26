4760c79195aa594ded1e500f0539c5d472448736
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-26 22:11:36 +0900

    logic

--- PrimeNumber/src/PrimeNumber/primeNumber.java
+++ PrimeNumber/src/PrimeNumber/primeNumber.java
@@ -1,10 +1,43 @@
 package PrimeNumber;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class primeNumber {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("aa");
+
+		primeNumberCheck();
 	}
 
+	public static void primeNumberCheck() {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String inputStr = null;
+		int inputNum = 0;
+		try {
+			inputStr = br.readLine();
+			inputNum = Integer.parseInt(inputStr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		int yakusuu = 0;
+		for (int i = 1; i <= inputNum; i++) {
+
+			yakusuu = 0;
+			for (int j = 1; j <= inputNum; j++) {
+
+				if (i % j == 0) {
+					yakusuu++;
+				}
+			}
+
+			if (yakusuu == 2) {
+				System.out.println(i + "は素数です");
+			}
+		}
+	}
 }
