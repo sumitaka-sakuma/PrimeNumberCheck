27d43f52ed0fa71c9107f98ea19f20d2f8c181c0
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-27 20:38:14 +0900

    create TestClass

--- PrimeNumber/src/PrimeNumber/PrimeNumber.java
+++ PrimeNumber/src/PrimeNumber/PrimeNumber.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class primeNumber {
+public class PrimeNumber {
 
 	public static void main(String[] args) {
 
---
+++ PrimeNumber/src/test/PrimeNumberTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import PrimeNumber.PrimeNumber;
+
+public class PrimeNumberTest {
+
+	private static PrimeNumber rn;
+
+	@BeforeClass
+	public static void getTestClass() {
+		rn = new PrimeNumber();
+	}
+
+	@Test
+	public void parseToInteger() {
+
+		//String str = "31";
+		//int except = 31;
+
+	}
+
+}
