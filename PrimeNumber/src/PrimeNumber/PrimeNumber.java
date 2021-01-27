7c0b618a26962a42edc3c7db0bfc34b89dff4e27
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-27 20:55:43 +0900

    parseToInteger

--- PrimeNumber/src/PrimeNumber/PrimeNumber.java
+++ PrimeNumber/src/PrimeNumber/PrimeNumber.java
@@ -10,19 +10,41 @@ public class PrimeNumber {
 
 	public static void main(String[] args) {
 
+		// 標準入力値変換処理
+		int inputNum = parseToInteger();
 		// 素数判定処理
-		boolean primeNumFlg = primeNumberCheck();
+		boolean primeNumFlg = primeNumberCheck(inputNum);
 
 	}
 
 	/*
-	 * 素数判定処理
-	 * @return primeNumFlg
+	 * 標準入力変換処理
+	 * @return inputNum
 	 */
-	public static boolean primeNumberCheck() {
+	public static int parseToInteger() {
 
 		// 標準入力
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String inputStr;
+		int inputNum = 0;
+		try {
+			// 入力、数値に変換
+			inputStr = br.readLine();
+			inputNum = Integer.parseInt(inputStr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return inputNum;
+	}
+
+	/*
+	 * 素数判定処理
+	 * @param inputNum
+	 * @return primeNumFlg
+	 */
+	public static boolean primeNumberCheck(int inputNum) {
+
 		// 素数判定フラグ
 		boolean primeNumFlg = false;
 		// 素数リスト
@@ -30,67 +52,56 @@ public class PrimeNumber {
 		// 合成数リスト
 		List<Integer> compositeNumList = new ArrayList<>();
 
-		String inputStr = null;
-		int inputNum = 0;
 		int yakusuu = 0;
-		try {
-			// 入力、数値に変換
-			inputStr = br.readLine();
-			inputNum = Integer.parseInt(inputStr);
 
-			// 2未満の素数は存在しないため処理を終了する
-			if (inputNum < 2) {
-				return primeNumFlg;
-			}
-
-			for (int i = 1; i <= inputNum; i++) {
-
-				// 1から順に割っていき割り切れるとき、yakusuuをインクリメント
-				yakusuu = 0;
-				for (int j = 1; j <= inputNum; j++) {
-					if (i % j == 0) {
-						yakusuu++;
-					}
-				}
-
-				// 入力された値が素数かを判定
-				if (inputNum == i) {
-					// 約数が２つの場合は、素数
-					if (yakusuu == 2) {
-						// 素数判定フラグをtrueにする
-						primeNumFlg = true;
-						primeNumList.add(i);
-					}
-				} else {
-					// 約数が２つの場合は、素数
-					if (yakusuu == 2) {
-						primeNumList.add(i);
-						continue;
-					}
-				}
-
-				// 合成数リストに格納
-				compositeNumList.add(i);
-			}
-
-			for (int primeNum : primeNumList) {
-				System.out.println("素数:" + primeNum);
-			}
-			for (int compositeNum : compositeNumList) {
-				System.out.println("合成数:" + compositeNum);
-			}
-
-			// 結果表示
-			if (primeNumFlg) {
-				System.out.println("素数です");
-			} else {
-				System.out.println("素数ではありません");
-			}
-
-		} catch (IOException | NumberFormatException e) {
-			e.printStackTrace();
+		// 2未満の素数は存在しないため処理を終了する
+		if (inputNum < 2) {
+			return primeNumFlg;
 		}
 
+		for (int i = 1; i <= inputNum; i++) {
+
+			// 1から順に割っていき割り切れるとき、yakusuuをインクリメント
+			yakusuu = 0;
+			for (int j = 1; j <= inputNum; j++) {
+				if (i % j == 0) {
+					yakusuu++;
+				}
+			}
+
+			// 入力された値が素数かを判定
+			if (inputNum == i) {
+				// 約数が２つの場合は、素数
+				if (yakusuu == 2) {
+					// 素数判定フラグをtrueにする
+					primeNumFlg = true;
+					primeNumList.add(i);
+				}
+			} else {
+				// 約数が２つの場合は、素数
+				if (yakusuu == 2) {
+					primeNumList.add(i);
+					continue;
+				}
+			}
+
+			// 合成数リストに格納
+			compositeNumList.add(i);
+		}
+
+		for (int primeNum : primeNumList) {
+			System.out.println("素数:" + primeNum);
+		}
+		for (int compositeNum : compositeNumList) {
+			System.out.println("合成数:" + compositeNum);
+		}
+
+		// 結果表示
+		if (primeNumFlg) {
+			System.out.println("素数です");
+		} else {
+			System.out.println("素数ではありません");
+		}
 		return primeNumFlg;
 	}
 }
